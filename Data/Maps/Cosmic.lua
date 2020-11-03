dd4ae6e95a3c59f558812a95725f3919fe181e4d
CruelDrool <aasmund.sater@gmail.com>
2020-11-03 18:24:23 +0100

    Updated NPC list for Darkmoon Faire.
    - Added Darkmoon Rabbit (58336)
    - Updated classifications for the others
    - Toys added to both Death Metal Knight (122899) and Moonfang (71992)
    - Pet added to Moonfang: Moon Moon (72160)

--- Data/Maps/Cosmic.lua
+++ Data/Maps/Cosmic.lua
@@ -9,6 +9,7 @@ local MapID = private.Enum.MapID
 -- Darkmon Island
 -- ----------------------------------------------------------------------------
 Maps[MapID.DarkmoonIsland].NPCs = {
+	[58336] = true, -- Darkmoon Rabbit
 	[71992] = true, -- Moonfang
 	[122899] = true, -- Death Metal Knight
 }
--- Data/NPCs/Cosmic.lua
+++ Data/NPCs/Cosmic.lua
@@ -3,15 +3,49 @@
 -- ----------------------------------------------------------------------------
 local AddOnFolderName, private = ...
 local NPCs = private.Data.NPCs
+local NPCClassification = private.Enum.NPCClassification
 
 -- ----------------------------------------------------------------------------
 -- Darkmoon Island
 -- ----------------------------------------------------------------------------
+NPCs[58336] = { -- Darkmoon Rabbit
+	classification = NPCClassification.Elite,
+	-- questID = ,
+	-- vignetteID = ,
+	pets = {
+		{
+			itemID = 80008, -- Darkmoon Rabbit
+			npcID = 59358, -- Darkmoon Rabbit
+		},
+	},
+}
+
 NPCs[71992] = { -- Moonfang
+	classification = NPCClassification.Elite,
 	vignetteID = 63,
+	toys = {
+		{ 
+			itemID = 101571, -- Moonfang Shroud
+		},
+		{ 
+			itemID = 105898, -- Moonfang's Paw
+		},
+	},
+	pets = {
+		{
+			itemID = 101570, -- Moon Moon
+			npcID = 72160, -- Moon Moon
+		},
+	},
 }
 
 NPCs[122899] = { -- Death Metal Knight
+	classification = NPCClassification.RareElite,
 	questID = 47767, -- Death Metal Knight
 	vignetteID = 2003,
+	toys = {
+		{ 
+			itemID = 151265, -- Blight Boar Microphone
+		},
+	},
 }
